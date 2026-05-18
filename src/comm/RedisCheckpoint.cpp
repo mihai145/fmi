3a185c9fa867902c7f07ebc88d70260e8b74d844
Mihai-Adrian Oprea <opreamihai01@yahoo.com>
2026-05-18 11:27:28 +0200

    send comm state upon termination as well

--- src/comm/RedisCheckpoint.cpp
+++ src/comm/RedisCheckpoint.cpp
@@ -2,9 +2,8 @@
 #include <boost/log/trivial.hpp>
 #include <cmath>
 
-FMI::Comm::RedisCheckpoint::RedisCheckpoint(
-    std::map<std::string, std::string> params,
-    std::map<std::string, std::string> model_params)
+FMI::Comm::RedisCheckpoint::RedisCheckpoint(std::map<std::string, std::string> params,
+                                            std::map<std::string, std::string> model_params)
     : ClientServer(params) {
     hostname = params["host"];
     port = std::stoi(params["port"]);
@@ -31,28 +30,27 @@ FMI::Comm::RedisCheckpoint::RedisCheckpoint(
         [this] { restore_fn(); });
 }
 
-FMI::Comm::RedisCheckpoint::~RedisCheckpoint() { teardown_fn(); }
+FMI::Comm::RedisCheckpoint::~RedisCheckpoint() {
+    checkpointer->shutdown_ctrl_thread();
+    checkpointer->register_hint(state);
+    teardown_fn();
+}
 
-void FMI::Comm::RedisCheckpoint::upload_object(channel_data buf,
-                                               std::string name) {
+void FMI::Comm::RedisCheckpoint::upload_object(channel_data buf, std::string name) {
     auto ctx = checkpointer->get_uninterruptible_context();
     std::string command = "SET " + name + " %b";
-    auto *reply =
-        (redisReply *)redisCommand(context, command.c_str(), buf.buf, buf.len);
+    auto *reply = (redisReply *)redisCommand(context, command.c_str(), buf.buf, buf.len);
     bool ok = reply->type != REDIS_REPLY_ERROR;
     if (!ok)
-        BOOST_LOG_TRIVIAL(error)
-            << "Error when uploading to Redis: " << reply->str;
+        BOOST_LOG_TRIVIAL(error) << "Error when uploading to Redis: " << reply->str;
     freeReplyObject(reply);
 }
 
-bool FMI::Comm::RedisCheckpoint::download_object(channel_data buf,
-                                                 std::string name) {
+bool FMI::Comm::RedisCheckpoint::download_object(channel_data buf, std::string name) {
     auto ctx = checkpointer->get_uninterruptible_context();
     std::string command = "GET " + name;
     auto *reply = (redisReply *)redisCommand(context, command.c_str());
-    bool ok =
-        reply->type != REDIS_REPLY_NIL && reply->type != REDIS_REPLY_ERROR;
+    bool ok = reply->type != REDIS_REPLY_NIL && reply->type != REDIS_REPLY_ERROR;
     if (ok)
         std::memcpy(buf.buf, reply->str, std::min(buf.len, reply->len));
     freeReplyObject(reply);
@@ -88,32 +86,24 @@ std::vector<std::string> FMI::Comm::RedisCheckpoint::get_object_names() {
         unsigned int barrier_num = num_operations.at("barrier") - 1;
         std::string barrier_suffix = "_barrier_" + std::to_string(barrier_num);
         for (int i = 0; i < state.num_peers; i++) {
-            std::string expected =
-                comm_name + std::to_string(i) + barrier_suffix;
-            state.waiting_for[i] =
-                std::find(keys.begin(), keys.end(), expected) == keys.end();
+            std::string expected = comm_name + std::to_string(i) + barrier_suffix;
+            state.waiting_for[i] = std::find(keys.begin(), keys.end(), expected) == keys.end();
         }
     }
 
     return keys;
 }
 
-double FMI::Comm::RedisCheckpoint::get_latency(Utils::peer_num producer,
-                                               Utils::peer_num consumer,
+double FMI::Comm::RedisCheckpoint::get_latency(Utils::peer_num producer, Utils::peer_num consumer,
                                                std::size_t size_in_bytes) {
-    double agg_bandwidth =
-        std::min(producer * consumer * bandwidth_single, bandwidth_multiple);
-    double trans_time = producer * consumer *
-                        ((double)size_in_bytes / 1000000.) / agg_bandwidth;
+    double agg_bandwidth = std::min(producer * consumer * bandwidth_single, bandwidth_multiple);
+    double trans_time = producer * consumer * ((double)size_in_bytes / 1000000.) / agg_bandwidth;
     return log2(producer + consumer) * overhead + trans_time;
 }
 
-double FMI::Comm::RedisCheckpoint::get_price(Utils::peer_num producer,
-                                             Utils::peer_num consumer,
+double FMI::Comm::RedisCheckpoint::get_price(Utils::peer_num producer, Utils::peer_num consumer,
                                              std::size_t size_in_bytes) {
-    double transfer_costs = (1 + consumer) * producer *
-                            ((double)size_in_bytes / 1000000000.) *
-                            transfer_price;
+    double transfer_costs = (1 + consumer) * producer * ((double)size_in_bytes / 1000000000.) * transfer_price;
     double total_costs = transfer_costs;
     if (include_infrastructure_costs) {
         total_costs += 1. / requests_per_hour * instance_price;
@@ -130,8 +120,7 @@ void FMI::Comm::RedisCheckpoint::restore_fn() {
     context = redisConnect(hostname.c_str(), port);
     if (context == nullptr || context->err) {
         if (context) {
-            BOOST_LOG_TRIVIAL(error)
-                << "Error when connecting to Redis: " << context->errstr;
+            BOOST_LOG_TRIVIAL(error) << "Error when connecting to Redis: " << context->errstr;
         } else {
             BOOST_LOG_TRIVIAL(error) << "Allocating Redis context not possible";
         }
